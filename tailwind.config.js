cf6d83e4778f33e30ed2be59ab5eb1f7968ece3b
Jon Dayton <jdayton@tract.eco>
2024-03-12 23:21:57 +0100

    add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,35 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
 
+const themeColor = require("./src/themeColors");
+
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{ts,tsx}"],
+  // content: [],
+  theme: {
+    extend: {
+      zIndex: {
+        1300: 1300,
+        1500: 1500,
+      },
+      colors: themeColor,
+      margin: {
+        76: 304,
+        68: 272,
+      },
+      screens: {
+        xl: "1280px",
+        "2xl": "1440px",
+        "3xl": "1630px",
+      },
+
+      dropShadow: {
+        table: "10px 0 0.3px rgba(203,213,225, 0.3)",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("@headlessui/tailwindcss")],
+};
